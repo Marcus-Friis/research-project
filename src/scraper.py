7863bde656ab6a8839c831e0c3cc528b60d13cfc
Marcus-Friis <marcus.friis99@gmail.com>
2023-09-22 16:16:54 +0200

    scraper to get metadata and pdf content from arxiv

--- src/scraper.py
+++ src/scraper.py
@@ -1,4 +1,3 @@
-from bs4 import BeautifulSoup
 import requests
 import io
 import PyPDF2
@@ -35,11 +34,33 @@ class PDFToStringConverter:
 class ArxivScraper:
     def __init__(self, arxiv_list: list) -> None:
         self.arxiv_list = arxiv_list
-
-    def _visit_article(self, arxiv_id: str) -> None:
+    
+    
+    def _get_body(self, arxiv_id: str) -> str:
         url = f'https://arxiv.org/pdf/hep-ph/{arxiv_id}.pdf'
         pdf = PDFToStringConverter(url)
         return pdf.convert_to_string()
+    
+    def _get_metadata(self, arxiv_id: str) -> str:
+        api_url = f'https://export.arxiv.org/api/query?id_list=hep-ph/{arxiv_id}&max_results=1'
+        response = requests.get(api_url)
+        return response.content
+        
+    def _visit_article(self, arxiv_id: str) -> str:
+        metadata = self._get_metadata(arxiv_id)
+        body = self._get_body(arxiv_id)
+        data = {
+            'metadata': metadata,
+            'body': body
+        }
+        return data
+
+    def scrape(self):
+        data = {}
+        for arxiv_id in self.arxiv_list:
+            arxiv_content = self._visit_article(arxiv_id)
+            data[arxiv_id] = arxiv_content
+        return data
 
 
 if __name__ == '__main__':
