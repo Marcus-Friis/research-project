c58e41cc463708d3e96947e5a346ef02a9113940
Marcus-Friis <marcus.friis99@gmail.com>
2023-09-27 14:32:04 +0200

    updated scraper, now caches intermediate results ⭐

--- src/scraper.py
+++ src/scraper.py
@@ -1,6 +1,9 @@
 import requests
 import io
 import PyPDF2
+import time
+import json
+import os
 
 
 class PDFToStringConverter:
@@ -32,9 +35,21 @@ class PDFToStringConverter:
 
 
 class ArxivScraper:
-    def __init__(self, arxiv_list: list) -> None:
-        self.arxiv_list = arxiv_list
+    filename = 'arxiv.json'
     
+    def _init_json(self):
+        if os.path.isfile(self.filename):
+            with open(self.filename, 'r') as f:
+                return json.load(f)
+        
+        data = {}
+        j = json.dumps(data, indent=4)
+        with open(self.filename, 'w') as f:
+            f.write(j)
+        return data
+    
+    def __init__(self) -> None:
+        self.data = self._init_json()    
     
     def _get_body(self, arxiv_id: str) -> str:
         url = f'https://arxiv.org/pdf/hep-ph/{arxiv_id}.pdf'
@@ -44,7 +59,7 @@ class ArxivScraper:
     def _get_metadata(self, arxiv_id: str) -> str:
         api_url = f'https://export.arxiv.org/api/query?id_list=hep-ph/{arxiv_id}&max_results=1'
         response = requests.get(api_url)
-        return response.content
+        return response.text
         
     def _visit_article(self, arxiv_id: str) -> str:
         metadata = self._get_metadata(arxiv_id)
@@ -54,18 +69,27 @@ class ArxivScraper:
             'body': body
         }
         return data
+    
+    def wipe_chache(self):
+        with open(self.filename, 'w') as f:
+            json.dump({}, f)
+        self.data = {}
 
-    def scrape(self):
-        data = {}
-        for arxiv_id in self.arxiv_list:
-            arxiv_content = self._visit_article(arxiv_id)
-            data[arxiv_id] = arxiv_content
-        return data
+    def scrape(self, arxiv_list: list, sleep_interval: int = 15) -> dict:
+        for arxiv_id in arxiv_list:
+            if arxiv_id not in self.data.keys():
+                arxiv_content = self._visit_article(arxiv_id)
+                self.data[arxiv_id] = arxiv_content
+                j = json.dumps(self.data, indent=4)
+                with open(self.filename, 'w') as f:
+                    f.write(j)
+                time.sleep(sleep_interval)
+        return self.data
 
 
 if __name__ == '__main__':
     arxiv_list = ['0203079']
-    arx = ArxivScraper(arxiv_list)
+    arx = ArxivScraper()
     print(arx._visit_article(arxiv_list[0]))
 
 
