0c24cbff1fdde05eb6c41d834e1f69402d687de8
marcus <marcus.friis99@gmail.com>
2023-10-24 11:38:57 +0200

    EMBEDDINGS

--- hpc/main.py
+++ hpc/main.py
@@ -1,15 +1,20 @@
 from llama_cpp import Llama
 
+# prepare model
 model_path = 'openbuddy-llama2-13b-v11.1.Q5_K_M.gguf'
 n_gpu_layers = -1
 embedding = True
 verbose = False
 llama = Llama(model_path=model_path, n_gpu_layers=n_gpu_layers, embedding=embedding, verbose=verbose)
 
-
+# define prompt
 prompt = 'Q: who is the most beautiful? Mads or Morten? A:'
 
+# generate text
 output = llama(prompt)
+print(output)
 
-print(output["choices"][0]["text"])
+# generate embedding
+embedding = llama.embed(prompt)
+print(embedding)
 
