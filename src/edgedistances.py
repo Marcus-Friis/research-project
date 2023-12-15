f69a27f4b0c17a604487310640ea5972136c81d3
Marcus Friis <mahf@itu.dk>
2023-12-15 07:40:57 +0100

    fixed memory leak

--- src/edgedistances.py
+++ src/edgedistances.py
@@ -1,23 +1,25 @@
-from graph_utilities import multilayer_lcc
+from graph_utilities import lcc_aug_embedding
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 
 if __name__ == '__main__':
-    _, g_pos, g_neu, g_neg = multilayer_lcc()
-    G = [g_pos, g_neu, g_neg]
+    from collections import defaultdict
+    data = defaultdict(int)
     
-    # compute average distance between nodes of the same label
+    g = lcc_aug_embedding(load_embeds=False)
+    for edge in g.es:
+        label = edge['label']
+        u = g.vs[edge.source]
+        v = g.vs[edge.target]
+        u_emb = np.array(u['embedding']).reshape(1, -1)
+        v_emb = np.array(v['embedding']).reshape(1, -1)
+        
+        data[label + '_euclidean'] += np.linalg.norm(u_emb - v_emb)
+        data[label + '_cosine'] += cosine_similarity(u_emb, v_emb)[0,0]
+        data[label + '_count'] += 1
+        
     labels = ('agreement', 'neutral', 'disagreement')
-    for i, g in enumerate(G):
-        print(labels[i])
-        distances = []
-        similarities = []
-        for edge in g.es:
-            u = g.vs[edge.source]
-            v = g.vs[edge.target]
-            u_emb = np.array(u['embedding']).reshape(-1, 1)
-            v_emb = np.array(v['embedding']).reshape(-1, 1)
-            distances.append(np.linalg.norm(u_emb - v_emb))
-            similarities.append(cosine_similarity(u_emb, v_emb))
-        print('\tEuclidean\t', np.mean(distances))
-        print('\tCosine   \t', np.mean(similarities))
+    for label in labels:
+        print(label)
+        print('\tEuclidean\t', data[label + '_euclidean'] / data[label + '_count'])
+        print('\tCosine   \t', data[label + '_cosine'] / data[label + '_count'])
